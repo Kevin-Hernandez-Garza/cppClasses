937236f56ac11bde5dec72e0f3265bdaf2c4da6c
Kevin Hernandez <kev.hernandezgarza@gmail.com>
2023-03-06 17:10:59 -0600

    setting default value if wrong values are passed by declaring set functions

--- KevinHernandezG_Lab4.cpp
+++ KevinHernandezG_Lab4.cpp
@@ -15,28 +15,61 @@
 
 using namespace std;
 
-// class declaration
+// instantiating the Population class
 class Population
 {
     // private and public specifiers
 private:
     // declaring member variables
-    int population = 2, births = 0, deaths = 0;
+    int population, births, deaths;
 
 public:
-    // birth rate function
+    // setting variables depending on input
+    void setPopulation(int);
+    void setBirthrate(int);
+    void setDeathrate(int);
+
+    // birthrate member inline function
     int getBirthrate(int births, int population)
     {
         int birthrate;
         birthrate = births / population;
     }
-    // death rate function
+    // deathrate member inline function
     int getDeathrate(int deaths, int population)
     {
         int deathrate;
         deathrate = deaths / population;
     }
 };
+
+// set functions incase invalid informations is passed
+void Population::setPopulation(int population)
+{
+    if (population < 2)
+    {
+        population = 2;
+        cout << "Error: Invalid population input!" << endl;
+    }
+};
+
+void Population::setBirthrate(int births)
+{
+    if (births < 0)
+    {
+        births = 0;
+        cout << "Error: Invalid births input!" << endl;
+    }
+}
+
+void Population::setDeathrate(int deaths)
+{
+    if (deaths < 0)
+    {
+        deaths = 0;
+        cout << "Error: Invalid deaths input!" << endl;
+    }
+}
 
 // main function
 int main()
