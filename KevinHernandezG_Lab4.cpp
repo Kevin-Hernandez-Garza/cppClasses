7e95009ff66a2e4010cd65402804f69ec82f19be
Kevin Hernandez <kev.hernandezgarza@gmail.com>
2023-03-06 16:02:56 -0600

    initial source file structure

---
+++ KevinHernandezG_Lab4.cpp
@@ -0,0 +1,21 @@
+//******************************************************************
+// Author: Kevin Hernandez-Garza
+// Assignment Number: Programming Assignment "4"
+// File Name: KevinHernandezG_Lab4.cpp
+// Course/Section: COSC 1337 Section -009
+// Due Date: 3/7/23
+// Instructor: Dr.Haytham Mohamed
+//
+// This program gathers information on the current population, annual births, deaths for a geographical area by implement a C++ class.
+//  ******************************************************************
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
